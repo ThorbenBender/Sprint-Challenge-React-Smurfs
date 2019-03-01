c3728db90e79c65399c3efdc6a2b472ff88d529e
Thorben Bender <bender.thorben0@gmail.com>
2019-03-01 09:30:47 +0100

    added router and route

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -17,8 +17,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route path="/smurf-form" render={() => <SmurfForm />}></Route>
+        <Route exact path="/" render={() => <Smurfs smurfs={this.state.smurfs}/>}></Route>
       </div>
     );
   }
